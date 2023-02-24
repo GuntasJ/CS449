1e765bede95ff953a7f155b00d36a3cc0bac4756
GuntasJ <guntasjammu@gmail.com>
2023-02-23 23:04:44 -0600

    Made progress on GUI

--- src/main/java/graphics/GamePanel.java
+++ src/main/java/graphics/GamePanel.java
@@ -1,37 +1,130 @@
 package graphics;
 
-import javax.swing.JCheckBox;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+
 
 public class GamePanel extends JPanel {
 
-    private JRadioButton radioButton;
-    private JCheckBox checkBox;
-    private JLabel label;
+    private final JPanel topPanel;
+    private final JPanel westPanel;
+    private final JPanel eastPanel;
+    private final JPanel centerPanel;
+    private final JPanel southPanel;
+    private final ButtonGroup topButtons;
+    private final JRadioButton gameTypeSimpleRadioButton;
+    private final JRadioButton gameTypeGeneralRadioButton;
+    private final JLabel buttonGroupLabel;
+    private final JLabel boardSizeLabel;
+    private final JTextField boardSizeTextField;
+
 
     GamePanel() {
-        radioButton = new JRadioButton();
-        checkBox = new JCheckBox();
-        label = new JLabel("testing");
-        label.setForeground(Color.WHITE);
-        label.setSize(300, 300);
-        setPreferredSize(GraphicConstants.PANEL_BOUNDS);
+        topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 13));
+        westPanel = new JPanel();
+        eastPanel = new JPanel();
+        centerPanel = new JPanel();
+        southPanel = new JPanel();
+        topButtons = new ButtonGroup();
+        buttonGroupLabel = new JLabel();
+        gameTypeGeneralRadioButton = new JRadioButton("General Game");
+        gameTypeSimpleRadioButton = new JRadioButton("Simple Game");
+        boardSizeLabel = new JLabel("Board Size: ");
+        boardSizeTextField = new JTextField();
+
+        //setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        setLayout(new BorderLayout());
+        setPreferredSize(GraphicConstants.GAME_PANEL_BOUNDS);
         setBackground(Color.BLACK);
-        add(radioButton);
-        add(checkBox);
-        add(label);
+        setUpTopPanelAndComponents();
+
+        westPanel.setBackground(Color.BLUE);
+        westPanel.setPreferredSize(new Dimension(200, 200));
+        add(westPanel, BorderLayout.WEST);
+
+        eastPanel.setBackground(Color.RED);
+        eastPanel.setPreferredSize(new Dimension(200, 200));
+        add(eastPanel, BorderLayout.EAST);
+
+        centerPanel.setBackground(Color.WHITE);
+        centerPanel.setPreferredSize(new Dimension(200, 200));
+        add(centerPanel, BorderLayout.CENTER);
+
+        southPanel.setBackground(Color.GRAY);
+        southPanel.setPreferredSize(new Dimension(100, 200));
+        add(southPanel, BorderLayout.SOUTH);
+
     }
 
+    private void setUpTopPanelAndComponents() {
+        //top panel
+        topPanel.setBackground(Color.GRAY);
+        topPanel.setPreferredSize(GraphicConstants.TOP_PANEL_BOUNDS);
+
+        //labels
+        buttonGroupLabel.setText("SOS: ");
+        buttonGroupLabel.setPreferredSize(GraphicConstants.BUTTON_GROUP_LABEL_BOUNDS);
+        boardSizeLabel.setPreferredSize(GraphicConstants.BOARD_SIZE_LABEL_BOUNDS);
+
+
+        //radio buttons
+        gameTypeSimpleRadioButton.setPreferredSize(GraphicConstants.RADIO_BUTTON_GAME_TYPE_BOUNDS);
+        gameTypeGeneralRadioButton.setPreferredSize(GraphicConstants.RADIO_BUTTON_GAME_TYPE_BOUNDS);
+
+        //Text field
+        boardSizeTextField.setPreferredSize(GraphicConstants.TEXT_FIELD_BOUNDS);
+        boardSizeTextField.setEditable(true);
+
+        //adding them
+
+        topPanel.add(buttonGroupLabel);
+
+
+        topButtons.add(gameTypeSimpleRadioButton);
+        topButtons.add(gameTypeGeneralRadioButton);
+        topPanel.add(gameTypeSimpleRadioButton);
+        topPanel.add(gameTypeGeneralRadioButton);
+
+
+        topPanel.add(boardSizeLabel);
+
+        topPanel.add(boardSizeTextField);
+
+        add(topPanel, BorderLayout.NORTH);
+    }
+
+    private void setUpMiddleLeftPanelAndComponents() {
+
+    }
+    private void setUpMiddleRightPanelAndComponents() {
+
+    }
+    private void setUpSOSPanel() {
+
+    }
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.WHITE);
-        g.drawLine(50, 50, 100, 100);
-        g.drawLine(100, 50, 150, 100);
-        g.drawLine(150, 50, 200, 100);
     }
+
+    private class SOSGameTile extends JPanel {
+
+        public SOSGameTile() {
+            this.setBackground(Color.BLACK);
+            this.setBorder(BorderFactory.createLineBorder(Color.WHITE, 5));
+        }
+    }
+
 }
--- src/main/java/graphics/GraphicConstants.java
+++ src/main/java/graphics/GraphicConstants.java
@@ -4,7 +4,33 @@ import java.awt.Dimension;
 
 public class GraphicConstants {
 
-    public static final int HEIGHT = 600;
-    public static final int WIDTH = 800;
-    public static final Dimension PANEL_BOUNDS = new Dimension(WIDTH, HEIGHT);
+    public static final int GAME_PANEL_HEIGHT = 600;
+    public static final int GAME_PANEL_WIDTH = 800;
+    public static final Dimension GAME_PANEL_BOUNDS = new Dimension(GAME_PANEL_WIDTH, GAME_PANEL_HEIGHT);
+
+    public static final int TOP_PANEL_HEIGHT = 50;
+    public static final int TOP_PANEL_WIDTH = 800;
+    public static final Dimension TOP_PANEL_BOUNDS = new Dimension(TOP_PANEL_WIDTH, TOP_PANEL_HEIGHT);
+
+    public static final int BUTTON_GROUP_LABEL_HEIGHT = 25;
+    public static final int BUTTON_GROUP_LABEL_WIDTH = 50;
+    public static final Dimension BUTTON_GROUP_LABEL_BOUNDS =
+            new Dimension(BUTTON_GROUP_LABEL_WIDTH, BUTTON_GROUP_LABEL_HEIGHT);
+
+    public static final int BOARD_SIZE_LABEL_HEIGHT = 25;
+    public static final int BOARD_SIZE_LABEL_WIDTH = 75;
+    public static final Dimension BOARD_SIZE_LABEL_BOUNDS =
+            new Dimension(BOARD_SIZE_LABEL_WIDTH, BOARD_SIZE_LABEL_HEIGHT);
+
+    public static final int RADIO_BUTTON_GAME_TYPE_HEIGHT = 15;
+    public static final int RADIO_BUTTON_GAME_TYPE_WIDTH = 150;
+    public static final Dimension RADIO_BUTTON_GAME_TYPE_BOUNDS =
+            new Dimension(RADIO_BUTTON_GAME_TYPE_WIDTH, RADIO_BUTTON_GAME_TYPE_HEIGHT);
+
+    public static final int TEXT_FIELD_HEIGHT = 25;
+    public static final int TEXT_FIELD_WIDTH = 25;
+    public static final Dimension TEXT_FIELD_BOUNDS = new Dimension(TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT);
+
+
+
 }
